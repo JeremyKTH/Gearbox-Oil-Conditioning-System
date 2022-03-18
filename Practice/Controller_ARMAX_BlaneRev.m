82553fc5db45df44733241d883aab02ee62fcc08
cbbrinkley4 <cbbrinkley4@gmail.com>
2022-03-18 15:37:13 +0100

    Controller now converts cont. pole placement to PID specific parameters

--- Practice/Controller_ARMAX_BlaneRev.m
+++ Practice/Controller_ARMAX_BlaneRev.m
@@ -19,8 +19,6 @@ data_train.TimeUnit = 'seconds';
 data_train.InputName = 'OTSV1';   data_train.InputUnit = 'Percentage';
 data_train.OutputName = 'TV12';   data_train.OutputUnit = 'Celsius';
 
-data_train = detrend(data_train);
-
 %% Testing Data
 data_test = iddata(y_test, u_test, Ts_ID);
 data_test.Name = 'Data_Test';
@@ -28,6 +26,15 @@ data_test.TimeUnit = 'seconds';
 data_test.InputName = 'OTSV1';   data_test.InputUnit = 'Percentage';
 data_test.OutputName = 'TV12';   data_test.OutputUnit = 'Celsius';
 
+%% Detrending Data
+T_train = getTrend(data_train);
+T_test = getTrend(data_test);
+T_train.InputOffset = 40;
+T_train.OutputOffset = 48;
+T_test.InputOffset = 40;
+T_test.OutputOffset = 48;
+
+data_train = detrend(data_train);
 data_test = detrend(data_test);
 
 cost_func = 'NRMSE';
@@ -65,7 +72,8 @@ zeros_Gp_disc = zero(Gp)
 figure(2)
 pzmap(Gp)
 
-Gp_ar_cont = d2c(Gp, 'zoh') %converting to cont. 
+Gp_cont = d2c(Gp, 'zoh') %converting to cont. 
+Gp_ss = tf2ss(Gp.Numerator{1},Gp.denominator{1});
 
 %% Best +/-40% Model: ARMAX
 % b0 = 3.264e-05;
@@ -81,14 +89,14 @@ Gp_ar_cont = d2c(Gp, 'zoh') %converting to cont.
 % pzmap(Gp_ar)
 %% Choose Poles (w_m, zeta_m) (w_o, zeta_o)
 %--- A_m ------
-w_m = 2;
+w_m = 4; %.1 - 4
 zeta_m = 1;
 
 p1_m = 2*exp(-zeta_m*w_m*Ts)*cos(w_m*Ts*sqrt(1-(zeta_m)^2)); %
 p0_m = exp(-2*zeta_m*w_m*Ts);
 
 %--- A_o ------
-w_o = 500; %min. of 14*w_m to be stable
+w_o = 100; %15
 zeta_o = zeta_m;
 
 p1_o = 2*exp(-zeta_o*w_o*Ts)*cos(w_o*Ts*sqrt(1-(zeta_o)^2));
@@ -143,42 +151,10 @@ equ4 = A_cl_c(5) == A_d_c(5); % z^0
 
 sol = solve([equ1, equ2, equ3, equ4], [S2, S1, S0, r0]);
 
-S2 = double(sol.S2)   %
-S1 = double(sol.S1)   % 
-S0 = double(sol.S0)   % 
-r0 = double(sol.r0)   % 
-
-
-%% MATLAB PID TUNER SUBSTITUTION
-% P = 0;
-% I = 0; 
-% D = 0;
-% N = 0;
-% 
-% S2 = P + D*N;
-% S1 = I*Ts + P*N*Ts-2*P-2*D*N;
-% S0 = P-P*N*Ts + I*Ts + I*N*Ts^2 + D*N;
-% ro = N*Ts-1;
-% 
-% % ----- Getting b & c terms for Simulink ------------
-% %b = "set point weight for proportional term
-% %c = "set point weight for deriviative term
-% 
-% syms b c z
-% G_ff = (z-1)*(z-1+N*Ts)*b*P+c*N*D*(z-1)^2+I*Ts*(z-1+N*Ts);
-% G_ff_c = fliplr(coeffs(G_ff, z)); % retreive coeficients
-% 
-% equ1 = G_ff_c(1) == T(1); % z^2
-% equ2 = G_ff_c(2) == T(2); % z^1
-% equ3 = G_ff_c(3) == T(3); % z^0
-% 
-% sol = solve([equ1, equ3], [b, c]);
-% 
-% b = double(sol.b)   % 0.4684
-% c = double(sol.c)   % 0.1157
-% 
-% % Get S for Simulink
-% [S, ~] = tfdata(Gc);
+S2 = double(sol.S2);   %
+S1 = double(sol.S1);   % 
+S0 = double(sol.S0);   % 
+r0 = double(sol.r0);   % 
 
 %% Controller TF - Gc (S/R)
 %z = tf('z', Ts);
@@ -205,6 +181,47 @@ T = t_o*A_o;
 R = [1, r0-1, r0];
 
 Gff = tf(T, R, Ts);
+
+%% MATLAB PID TUNER SUBSTITUTION
+
+% (EULER FORWARD)
+syms P I D N 
+equ1 = S2 == P + (D*N);
+equ2 = S1 == (I*Ts) + (P*N*Ts) - (2*P) - (2*D*N);
+equ3 = S0 == (P-P*N*Ts) + (I*Ts) + (I*N*Ts^2) + (D*N);
+equ4 = r0 == N*Ts-1;
+
+sol = solve([equ4, equ1, equ2, equ3], [N, P, I, D]);
+
+N = double(sol.N)
+P = double(sol.P)
+I = double(sol.I)
+D = double(sol.D)
+
+% ----- Getting b & c terms for Simulink ------------
+%b = "set point weight for proportional term
+%c = "set point weight for deriviative term
+
+syms b c z
+% EULEER FORWARD VERSION:
+G_ff_PID = (P*b*(z-1)*(z-1+N*Ts)) + (I*Ts*(z-1+N*Ts)) + (D*N*c*(z-1)^2);
+
+% EULER BACKWARD VERSION
+%G_ff_PID = (P*b*(z-1)*(z-1+N*Ts*z)) + (I*Ts*(z-1+N*Ts*z)) + (D*N*c*(z-1)^2);
+
+G_ff_c = fliplr(coeffs(G_ff_PID, z)); % retreive coeficients
+
+equ1 = G_ff_c(1) == T(1); % z^2
+equ2 = G_ff_c(2) == T(2); % z^1
+equ3 = G_ff_c(3) == T(3); % z^0
+
+sol = solve([equ1, equ3], [b, c]);
+
+b = double(sol.b)  
+c = double(sol.c)  
+ 
+% % Get S for Simulink
+% [S, ~] = tfdata(Gc);
 
 %% Entire closed loop check (Gyr)
 Gyr = Gff*Gp/(1+Gc*Gp)
@@ -245,7 +262,7 @@ PD_SR_num = [d1_Gc, d0_Gc];
 PD_TR_num = [d1_Gff, d0_Gff];
 PD_den = [1, r0];
 
-K_ant = 10; %antiwindup coefficitnt
+K_ant = 4; %antiwindup coefficitnt STARTS: %2.5, BREAKS:6
 
 %% Sensitivy Analysis
 
